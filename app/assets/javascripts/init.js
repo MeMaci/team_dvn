25aa66ed7637d5b33aae9c790fe1d8f043fb5103
Melike Macineiras <mel.sensivas@gmail.com>
2017-05-31 22:12:49 -0400

    added carousel

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -3,6 +3,8 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+    $('.carousel').carousel();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
